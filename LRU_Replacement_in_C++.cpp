8a34181687043ec9dc0cd67b98bfd19d5d8e2e3b
Haig Nalbandian <hnalband@usc.edu>
2015-05-05 18:01:30 -0700

    replacement strategy in c++

---
+++ LRU_Replacement_in_C++.cpp
@@ -0,0 +1,15 @@
+data int[64][4]
+randomAddresses int[1000]
+
+for (auto address: randomAddresses){
+	int testRow = address%64;
+	for(int i=0; i < 4; i++){
+		if(data[testRow][i]==address||i==3){
+			for(int j=1; j<=i; j++){
+				data[testRow][j] = data[testRow][j-1];
+			}
+			data[testRow][0] = address;
+			break;
+		}
+	}
+}
