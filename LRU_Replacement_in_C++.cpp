b6a43f32e6d38064dc96e7c1e5e22500c47e92bb
Haig Nalbandian <hnalband@usc.edu>
2015-05-05 18:10:04 -0700

    added hit and miss tracking

--- LRU_Replacement_in_C++.cpp
+++ LRU_Replacement_in_C++.cpp
@@ -1,10 +1,18 @@
-data int[64][4]
-randomAddresses int[1000]
+data int[64][4];
+randomAddresses int[1000];
+
+int hit;
+int miss;
 
 for (auto address: randomAddresses){
 	int testRow = address%64;
 	for(int i=0; i < 4; i++){
 		if(data[testRow][i]==address||i==3){
+			if(data[testRow][i]==address){
+				hit++;
+			}else{
+				miss++;
+			}
 			for(int j=1; j<=i; j++){
 				data[testRow][j] = data[testRow][j-1];
 			}
